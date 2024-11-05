ee2802cd2954cb91f6e247140606b77a6d364b36
Perez Benjamin <benjamin.perez@etu.univ-orleans.fr>
2024-11-05 12:00:22 +0100

    question 4

--- tp3/bonnes_lectures/models.py
+++ tp3/bonnes_lectures/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255, blank=False, null=True)
@@ -7,3 +9,9 @@ class Book(models.Model):
     isbn = models.CharField(max_length=255, blank=False, null=True)
     backcover = models.TextField(blank=False, null=True)
     cover = models.ImageField(upload_to='images/', blank=True, null=True)
+
+class Review(models.Model):
+    date = models.DateField(blank=False)
+    text = models.TextField(blank=False, null=True)
+    note = models.IntegerField(blank=False, null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
